f817f02860d1b08245ec1dd3bcf4c81ea9d2fec5
rick6502 <rick2032@hotmail.com>
2017-02-21 22:04:40 -0800

    Made changes to the HomeController to demo it works

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace PartyInvites.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Hello World";
         }
     }
 }
