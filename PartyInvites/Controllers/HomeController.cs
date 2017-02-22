f6f8f3101110ee5f4a8cb353eb797981b602057d
rick6502 <rick2032@hotmail.com>
2017-02-21 22:34:37 -0800

    Modified the static view to be more dynamic

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -11,6 +11,10 @@ namespace PartyInvites.Controllers
         // GET: Home
         public ViewResult Index()
         {
+
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
             return View();
         }
     }
